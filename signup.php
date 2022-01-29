75b8f0df6f97385ee283f3c1828772dd313481da
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-29 11:00:17 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,9 @@
-
+<?php
+$SERVER="remotemysql.com";
+    $username="zM4DxHP5rI";
+    $password="BPC16e7X6D";
+    $con=mysqli_connect($SERVER,$username,$password);
+    if(!$con){
+        die("Connection to this database failed due to".mysqli_connect_error());
+    }
+?>
