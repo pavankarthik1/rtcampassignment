e277a0ec79f1777117f5d29ebc42ff2a74474c0a
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-29 13:42:36 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -9,9 +9,9 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // https://github.com/sendgrid/sendgrid-php/releases
 
 $email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("test@example.com", "Example User");
+$email->setFrom("xkcd038@gmail.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("test@example.com", "Example User");
+$email->addTo("solletyketankumar@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
