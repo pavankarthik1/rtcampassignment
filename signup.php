a6b219c5cd5a200731f1bc7a5fc0e97393347c3c
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-29 11:13:03 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,7 +3,17 @@ $SERVER="remotemysql.com";
     $username="zM4DxHP5rI";
     $password="BPC16e7X6D";
     $con=mysqli_connect($SERVER,$username,$password);
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $mail=$_POST['email'];
     if(!$con){
         die("Connection to this database failed due to".mysqli_connect_error());
     }
+    $sql = "INSERT INTO xkcd(firstname, lastname, email)
+VALUES ($fname, $lname, $email)";
+if ($con->query($sql) === TRUE) {
+  echo "Verification Mail sent succuessfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 ?>
