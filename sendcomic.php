454d341a796ad0cd7d638462591ff6cc6db3c7cc
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-30 17:25:59 +0530

    Update sendcomic.php

--- sendcomic.php
+++ sendcomic.php
@@ -5,5 +5,43 @@ $api_url="https://xkcd.com/614/info.0.json";
 $json_data = file_get_contents($api_url);
 $response_data = json_decode($json_data,true);
 $link =  $response_data["img"]; 
-echo $link;
+$headers = array(
+        "Authorization: Bearer ".getenv('Api'),
+        'Content-Type: application/json'
+    );
+
+    $data=array(
+        'personalizations'=>array(
+            array(
+                'to'=>array(
+                    array(
+                        'email'=>'pavankarthik960@gmail.com'
+                    )
+                )
+            )
+        ),
+        'from'=>array(
+            'email'=>'xkcd038@gmail.com'
+        ),
+        'subject'=>'Hi',
+        'content'=> array(
+            array(
+                'type'=>'text/html',
+                'value'=>'Hey man<br> <img src=$link>'
+            )
+        )
+
+    );
+    //'{"personalizations": [{"to": [{"email": "solletyketankumar@gmail.com"}]}],"from": {"email": "xkcd038@gmail.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.sendgrid.com/v3/mail/send');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    
+
+    curl_close($ch);
 ?>
