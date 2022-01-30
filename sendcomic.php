c9382315f218953e9a1363c70f69bbf8b33cc1e2
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-30 17:21:23 +0530

    Update sendcomic.php

--- sendcomic.php
+++ sendcomic.php
@@ -3,7 +3,7 @@ echo "Hello";
 $con=mysqli_connect(getenv('SERVER'),getenv('dbusername'),getenv('dbpassword'),getenv('dbname'));
 $api_url="https://xkcd.com/614/info.0.json";
 $json_data = file_get_contents($api_url);
-$response_data = json_decode($json_data);
+$response_data = json_decode($json_data,true);
 $link =  $response_data["img"]; 
 echo $link;
 ?>
