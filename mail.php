766d049fe1236b22dc9f84d0982d9c7e71e5dad3
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-30 11:36:58 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,27 +1,40 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+$headers = array(
+                "Authorization: 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",
+                'Content-Type: application/json'
+            );
+ $data=array(
+        'personalizations'=>array(
+            array(
+                'to'=>array(
+                    array(
+                        'email'=>'pavankarthik960@gmail.com'
+                    )
+                )
+            )
+        ),
+        'from'=>array(
+            'email'=>'xkcd038@gmail.com'
+        ),
+        'subject'=>'Hi',
+        'content'=> array(
+            array(
+                'type'=>'text/html',
+                'value'=>'Hey man'
+            )
+        )
 
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("xkcd038@gmail.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("solletyketankumar@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+    );
+    echo $key;
+    //'{"personalizations": [{"to": [{"email": "solletyketankumar@gmail.com"}]}],"from": {"email": "xkcd038@gmail.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.sendgrid.com/v3/mail/send');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    echo $response;
+    curl_close($ch);
 }
