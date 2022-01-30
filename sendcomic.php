0b1ccf4a6bf0a1c64a057b46244ddc488817fc37
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-30 16:58:35 +0530

    Create sendcomic.php

---
+++ sendcomic.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect(getenv('SERVER'),getenv('dbusername'),getenv('dbpassword'),getenv('dbname'));
+$api_url="https://xkcd.com/614/info.0.json";
+$json_data = file_get_contents($api_url);
+$response_data = json_decode($json_data);
+$user_data = $response_data['img'];
+echo $user_data;
+?>
