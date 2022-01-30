7aecba055c7f7bc44d5b899cf7c19916666094de
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-30 16:51:03 +0530

    Create unsubscribe.php

---
+++ unsubscribe.php
@@ -0,0 +1,28 @@
+<?php
+$con=mysqli_connect(getenv('SERVER'),getenv('dbusername'),getenv('dbpassword'),getenv('dbname'));
+if(isset($_GET['vkey']))
+    {
+        $vkey=$_GET['vkey'];
+        $result=$con->query("select `activation`,`verifykey` from `xkcd` where `activation`= 0 and `verifykey`='$vkey' LIMIT 1 ");
+        if($result->num_rows==1)
+        {
+            $update =$con->query("update `xkcd` set activation =1 where `verifykey`='$vkey' LIMIT 1");
+            if($update)
+            {
+                echo "You are succefully unsubscibed";
+            }
+            else
+            {
+                echo $con->error;
+            }
+        }
+        else
+        {
+            echo "This account is invalid or already verified.";
+        }
+    }
+    else
+    {
+        die("Something went wrong");
+    }
+?>
