6f624298e062d3dd019d8fed4f62537b795ff441
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-31 14:33:06 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,44 +1,82 @@
 <?php
-    
+    class SendEmail{
+
+        public static function SendMail($from,$to,$subject,$content,$key,$attachment = NULL){
     
     $headers = array(
         "Authorization: Bearer ".getenv('Api'),
         'Content-Type: application/json'
     );
 
-    $data=array(
-        'personalizations'=>array(
-            array(
-                'to'=>array(
-                    array(
-                        'email'=>'pavankarthik960@gmail.com'
+    if($attachment == NULL){
+                $data = array(
+                    'personalizations' => array(
+                        array(
+                            'to' => array (
+                                array(
+                                    'email' => $to,
+                                )
+                            )
+                        )
+                    ),
+                    'from' => array(
+                        'email' => $from
+                    ),
+                    'subject' => $subject,
+                    'Content' => array(
+                        array(
+                            'type' => 'text/html',
+                            'value' => $content
+                        )
                     )
-                )
-            )
-        ),
-        'from'=>array(
-            'email'=>'xkcd038@gmail.com'
-        ),
-        'subject'=>'Hi',
-        'content'=> array(
-            array(
-                'type'=>'text/html',
-                'value'=>'Hey man'
-            )
-        )
+                );   
+            }else{
+                $data = array(
+                    'personalizations' => array(
+                        array(
+                            'to' => array (
+                                array(
+                                    'email' => $to,
+                                )
+                            )
+                        )
+                    ),
+                    'from' => array(
+                        'email' => $from
+                    ),
+                    'subject' => $subject,
+                    'Content' => array(
+                        array(
+                            'type' => 'text/html',
+                            'value' => $content
+                            )
+                        ),
+                    'attachments' => array(
+                        array(
+                            'content' => $attachment,
+                            'type' => 'image/jpeg',
+                            'filename' => 'comic',
+                            'disposition' => 'attachment',
+                            'content_ID' => 'image-attachment',
+                        ),
+                    ) 
+                );
+            }
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://api.sendgrid.com/v3/mail/send');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($ch);
+            curl_close($ch);
 
-    );
-    //'{"personalizations": [{"to": [{"email": "solletyketankumar@gmail.com"}]}],"from": {"email": "xkcd038@gmail.com"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}'
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://api.sendgrid.com/v3/mail/send');
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $response = curl_exec($ch);
-    
-
-    curl_close($ch);
-    
+            if($response != ''){
+                return FALSE;
+            }else{
+                return TRUE;
+            }
+        }
+    }
 ?>
