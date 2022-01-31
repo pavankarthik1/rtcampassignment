be2713adef918d7b5c5cdbdd5abe2380f80815c2
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-01-31 18:17:10 +0530

    Update unsubscribe.php

--- unsubscribe.php
+++ unsubscribe.php
@@ -3,7 +3,7 @@ $con=mysqli_connect(getenv('SERVER'),getenv('dbusername'),getenv('dbpassword'),g
 if(isset($_GET['vkey']))
     {
         $vkey=$_GET['vkey'];
-        $result=$con->query("select `activation`,`verifykey` from `xkcd` where `activation`= 0 and `verifykey`='$vkey' LIMIT 1 ");
+        $result=$con->query("select `activation`,`verifykey` from `xkcd` where `activation`= 1 and `verifykey`='$vkey' LIMIT 1 ");
         if($result->num_rows==1)
         {
             $update =$con->query("update `xkcd` set activation =0 where `verifykey`='$vkey' LIMIT 1");
