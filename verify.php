344e549643898d59f4b06a115b318c33c148261b
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-02 19:39:38 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -9,20 +9,21 @@
             $update =$con->query("update `xkcd` set activation =1 where `verifykey`='$vkey' LIMIT 1");
             if($update)
             {
-                echo '<style >
-	div {
-  border: 2px solid black;
-  box-shadow: 10px 10px 8px 10px #888888;
-    margin: auto;
-  width: 50%;
-    height: 180px;
-  width: 350px;
-}
-</style>
-<div align="center" >
-	<img src="index.png " height =80 width=80/>
-	<h1 style="font-family:verdana;">You have been subscibed</h1>
-</div>';
+                echo '
+<div style="width: 200px;
+    height: 200px;
+    
+    color: #fff;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    box-sizing: border-box;
+    padding: 70px 30px;
+    background: linear-gradient(to bottom, #00ff00 0%, #99ff99 100%);
+                border-radius: 10px;">
+                <h1>You have been subscribed</h1>
+                </div>';
             }
             else
             {
