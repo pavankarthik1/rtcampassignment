13838a4bbee305c6a3bdb27a77432ebf9699ce6a
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-02 21:33:49 +0530

    Update unsubscribe.php

--- unsubscribe.php
+++ unsubscribe.php
@@ -9,7 +9,28 @@ if(isset($_GET['vkey']))
             $update =$con->query("DELETE FROM `xkcd` WHERE verifykey='$vkey'");
             if($update)
             {
-                echo "You are succefully unsubscibed";
+               echo '<style>body{
+    margin: 0;
+    padding: 0;
+    background-repeat: repeat-x;
+    background-size: cover;
+    background-position: center;
+    font-family: sans-serif;
+}</style><body background="unsplash01.jpg"></body>
+<div style="width: 200px;
+    height: 200px;
+    
+    color: #fff;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    box-sizing: border-box;
+    padding: 70px 30px;
+    background: linear-gradient(to bottom, #00ff00 0%, #99ff99 100%);
+                border-radius: 10px;">
+                <h1>You have been unsubscribed</h1>
+                </div>';
             }
             else
             {
