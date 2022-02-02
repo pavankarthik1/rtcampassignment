826158b10d3c4e6d21c21b56b9d034af0a75802d
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-02 19:14:21 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,14 +1,22 @@
 <?php
     include 'mail.php';
+    $flag=0;
     $con=mysqli_connect(getenv('SERVER'),getenv('dbusername'),getenv('dbpassword'),getenv('dbname'));
+    if(!$con){
+        die("Connection to this database failed due to".mysqli_connect_error());
+    }
     $fname=filter_var($_POST['fname'], FILTER_SANITIZE_STRING);
     echo $fname;
     $lname=filter_var($_POST['lname'], FILTER_SANITIZE_STRING);
     $mail=filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $query='select * from xkcd where email=?';
+    $stmt=$con->prepare($query);
+    $stmt->bind_param('s',$mail);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt->num_rows==0){
     $verifykey=md5(time().$mail);
-    if(!$con){
-        die("Connection to this database failed due to".mysqli_connect_error());
-    }
+    
     $sql = "INSERT INTO xkcd(firstname,lastname,email,verifykey)
 VALUES ('$fname','$lname','$mail','$verifykey')";
 if ($con->query($sql) === TRUE) {
@@ -16,5 +24,9 @@ if ($con->query($sql) === TRUE) {
 
 $message="Please Verify Your mail address for recieving comics<a href='https://pavanrtcampassignemnt.herokuapp.com/verify.php?vkey=$verifykey'>Verify Account</a>";
  $email1 = SendMail(getenv('emailfrom'),$mail,'Email verification for comic book',$message,NULL);
-
+    }
+else
+{
+    echo "You have already been Subscibed";
+}
 ?>
