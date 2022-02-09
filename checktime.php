8e26f15bada4060b1ce928cab370927c738ecba9
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-09 18:41:40 +0530

    Update checktime.php

--- checktime.php
+++ checktime.php
@@ -4,7 +4,7 @@ $date = date('Y-m-d H:i:s');
 echo "Hello";
 $con=mysqli_connect(getenv('SERVER'), getenv('dbusername'), getenv('dbpassword'), getenv('dbname'));
 echo "Hello";
-$update=$con->query("INSERT INTO xkcd1(date) VALUES ($date)");
+$update=$con->query('INSERT INTO xkcd1(date) VALUES ($date)');
 echo $update;
 echo "The current server timezone is: " . $timezone. $date;
 
