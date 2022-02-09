0ff34390166ced46752169083dcbaecc331b2a1b
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-09 20:24:18 +0530

    Update checktime.php

--- checktime.php
+++ checktime.php
@@ -3,7 +3,7 @@ $timezone = date_default_timezone_get();
 $date=new DateTime(date('Y-m-d H:i:s'));
 $res=$date->format('Y-m-d H:i:s');
 $con=mysqli_connect(getenv('SERVER'), getenv('dbusername'), getenv('dbpassword'), getenv('dbname'));
-$update=$con->query("INSERT INTO xkcd1(date) VALUES ('$res')");
+#$update=$con->query("INSERT INTO xkcd1(date) VALUES ('$res')");
 /*
    $sql = "INSERT INTO xkcd1(date) VALUES (?)";
     $stmt1 = $con->prepare($sql);
@@ -14,6 +14,7 @@ $stmt1 = $con->prepare($res);
 $stmt1->bind_result($vas);
 $stmt1->execute();
 $stmt1->fetch();
+echo $vas;
 //$date1=new DateTime($vas);
 //echo $date1;
 //$date2=new DateTime($date);
@@ -23,8 +24,8 @@ $date_fetch = strtotime($vas);
 $date_fetch1 = getDate($date_fetch1); 
 echo gettype($date_input1);
 echo gettype($date_fetch1);*/
-#$diff=$date1->diff($date2);
-#echo $diff->format('Y-m-d H:i:s');
+$differ=$vas->diff($date);
+echo $differ->format('Y-m-d H:i:s');
 
 echo "The current server timezone is: " . $timezone. $date;
 
