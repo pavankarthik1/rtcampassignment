d4551ff2ab9800eb5ed74d6a3cf6b7bc9a280772
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-09 17:16:15 +0530

    Update verify.php

--- verify.php
+++ verify.php
@@ -12,6 +12,7 @@ if (isset($_GET['vkey'])) {
      $stmt1->store_result();
     if ($stmt1->num_rows == 1) {
         $stmt1->close();
+        echo "Working";
         $query="update `xkcd` set activation =1 where `verifykey`=? LIMIT 1";
         $stmt = $con->prepare($query);
         $stmt->bind_param('s', $vkey);
