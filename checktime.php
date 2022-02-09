87513cedad18900a0c5644ff2a5930a395da34ae
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-09 18:04:26 +0530

    Update checktime.php

--- checktime.php
+++ checktime.php
@@ -1,4 +1,5 @@
 <?php
 $timezone = date_default_timezone_get();
-echo "The current server timezone is: " . $timezone;
+$date = date('Y-m-d H:i:s');
+echo "The current server timezone is: " . $timezone. $date;
 ?>
