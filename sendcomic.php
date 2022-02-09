c46a46ab823d6d1998654b1ef79cd28ce098a44b
pavankarthik1 <44573797+pavankarthik1@users.noreply.github.com>
2022-02-09 23:05:31 +0530

    Update sendcomic.php

--- sendcomic.php
+++ sendcomic.php
@@ -7,15 +7,12 @@ $con=mysqli_connect(getenv('SERVER'), getenv('dbusername'), getenv('dbpassword')
 $api='https://c.xkcd.com/random/comic/';
 $head=get_headers($api);
 $n=parse_url(substr($head[15], 10))['path'];
-print_r($n);
 $r=rand(1, str_replace('/', '', $n));
-
 $api_url="https://xkcd.com/${r}/info.0.json";
 $json_data = file_get_contents($api_url);
 $response_data = json_decode($json_data, true);
 $link =  $response_data["img"]; 
 $link1=base64_encode(file_get_contents($link));// phpcs:ignore 
-
 $sql = $con->prepare("select * from xkcd where activation = 1");
 $sql->bind_result($fname, $lname, $email, $verifykey,$activation);
 $sql->execute();
